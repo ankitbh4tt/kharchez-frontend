33099d11f0849c42832f898a37034994b4fa4228
Ankit <brijtest09@gmail.com>
2025-03-22 08:44:16 +0530

    Modified API url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,1 @@
-export const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'http://your-server-ip-or-domain' // Replace with your server IP/domain
-    : 'https://kharchez-backend-153079716048.us-central1.run.app';
+export const API_BASE_URL = 'https://kharchez-backend-153079716048.us-central1.run.app';
